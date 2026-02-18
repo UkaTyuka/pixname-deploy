04f14a12cf44904d1574d6b0e01a3221df5d32a7
UkaTyuka <114570223+UkaTyuka@users.noreply.github.com>
2026-02-18 13:57:27 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,6 @@
-terraform {
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.54"
-    }
-  }
+provider "openstack" {
+  auth_url    = var.auth_url
+  user_name   = var.user_name
+  password    = var.password
+  tenant_name = var.tenant_name
 }
