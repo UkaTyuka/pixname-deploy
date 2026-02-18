d068ecbe1f4920c6c9cb026988e5572580112b96
UkaTyuka <114570223+UkaTyuka@users.noreply.github.com>
2026-02-18 13:31:28 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,28 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = ">= 1.54.0"
-    }
-  }
-}
-
 provider "openstack" {
-  auth_url    = var.os_auth_url
-  tenant_name = var.os_tenant_name
-  user_name   = var.os_username
-  password    = var.os_password
-  region      = var.os_region
-  domain_name = var.os_domain_name
+  auth_url    = var.auth_url
+  user_name   = var.user_name
+  password    = var.password
+  tenant_name = var.tenant_name
+  domain_name = var.domain_name
+  region      = var.region
 }
 
-resource "openstack_compute_keypair_v2" "jenkins" {
-  name       = "${var.name_prefix}-key"
-  public_key = var.ssh_public_key
+data "openstack_images_image_v2" "img" {
+  name        = var.image_name
+  most_recent = true
 }
 
-resource "openstack_networking_secgroup_v2" "app" {
-  name        = "${var.name_prefix}-sg"
-  description = "Allow ssh and app ports"
+data "openstack_compute_flavor_v2" "flavor" {
+  name = var.flavor_name
+}
+
+data "openstack_networking_network_v2" "net" {
+  name = var.network_name
+}
+
+resource "openstack_networking_secgroup_v2" "sg" {
+  name        = "${var.vm_name}-sg"
+  description = "pixname security group"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "ssh" {
@@ -33,46 +31,50 @@ resource "openstack_networking_secgroup_rule_v2" "ssh" {
   protocol          = "tcp"
   port_range_min    = 22
   port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.app.id
+  remote_ip_prefix  = var.allow_ssh_cidr
+  security_group_id = openstack_networking_secgroup_v2.sg.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "http" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
-  port_range_min    = var.app_port
-  port_range_max    = var.app_port
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.app.id
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = var.allow_http_cidr
+  security_group_id = openstack_networking_secgroup_v2.sg.id
 }
 
-# Если у тебя бот/графана/что-то еще — добавь правила под 3000, 8000 и т.д.
-
-data "template_file" "cloud_init" {
-  template = file("${path.module}/cloud-init.yaml")
+resource "openstack_networking_secgroup_rule_v2" "https" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = var.allow_http_cidr
+  security_group_id = openstack_networking_secgroup_v2.sg.id
 }
 
 resource "openstack_compute_instance_v2" "vm" {
-  name            = "${var.name_prefix}-vm"
-  image_name      = var.image_name
-  flavor_name     = var.flavor_name
-  key_pair        = openstack_compute_keypair_v2.jenkins.name
-  security_groups = [openstack_networking_secgroup_v2.app.name]
+  name            = var.vm_name
+  image_id        = data.openstack_images_image_v2.img.id
+  flavor_id       = data.openstack_compute_flavor_v2.flavor.id
+  key_pair        = var.keypair_name
+  security_groups = [openstack_networking_secgroup_v2.sg.name]
 
   network {
-    name = var.network_name
+    uuid = data.openstack_networking_network_v2.net.id
   }
-
-  user_data = data.template_file.cloud_init.rendered
 }
 
-# Плавающий IP (если используешь floating ip)
+# Floating IP из пула (часто pool называется "public" / "ext-net" / "external")
+variable "floating_ip_pool" { default = "public" }
+
 resource "openstack_networking_floatingip_v2" "fip" {
   pool = var.floating_ip_pool
 }
 
-resource "openstack_compute_floatingip_associate_v2" "fip_assoc" {
+resource "openstack_compute_floatingip_associate_v2" "assoc" {
   floating_ip = openstack_networking_floatingip_v2.fip.address
   instance_id = openstack_compute_instance_v2.vm.id
 }
