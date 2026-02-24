37dcf7a2bbc1c65542667eb4effe2fbae5afa300
UkaTyuka <114570223+UkaTyuka@users.noreply.github.com>
2026-02-24 15:06:22 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.54"
+      version = ">= 1.50.0"
     }
   }
 }
@@ -12,4 +12,21 @@ provider "openstack" {
   user_name   = var.user_name
   password    = var.password
   tenant_name = var.tenant_name
+  region      = var.region
+}
+
+resource "openstack_compute_instance_v2" "vm" {
+  name            = "pixname-vm"
+  image_name      = var.image_name
+  flavor_name     = var.flavor_name
+  key_pair        = var.key_pair_name
+  security_groups = ["default"]
+
+  network {
+    name = var.network_name
+  }
+}
+
+output "public_ip" {
+  value = openstack_compute_instance_v2.vm.access_ip_v4
 }
