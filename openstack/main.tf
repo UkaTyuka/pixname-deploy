2e2efe48f6016034063f2014dce4acbfda0889f0
UkaTyuka <114570223+UkaTyuka@users.noreply.github.com>
2026-02-25 05:12:48 +0300

    Update main.tf

--- openstack/main.tf
+++ openstack/main.tf
@@ -26,8 +26,8 @@ resource "openstack_compute_keypair_v2" "img_description" {
 
 # ----- ВМ под ELK / API -----
 
-resource "openstack_compute_instance_v2" "Pixname-vm" {
-  name        = "Pixname-vm"
+resource "openstack_compute_instance_v2" "pixname-vm" {
+  name        = "pixname-vm"
   image_name  = var.image_name
   flavor_name = var.flavor_name
   key_pair    = openstack_compute_keypair_v2.img_description.name
