abc99e5f6d97aadfe61af7818b4b3d4015050d54
UkaTyuka <114570223+UkaTyuka@users.noreply.github.com>
2026-02-25 05:12:27 +0300

    Update outputs.tf

--- openstack/outputs.tf
+++ openstack/outputs.tf
@@ -1,4 +1,4 @@
-output "Pixname-terraform_ip" {
+output "pixname-vm" {
   description = "IP-адрес ВМ в сети sutdents-net"
-  value       = openstack_compute_instance_v2.Pixname-terraform.network[0].fixed_ip_v4
+  value       = openstack_compute_instance_v2.pixname-vm.network[0].fixed_ip_v4
 }
